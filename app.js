2d983b5e63ec20b78d67a3ec169d7852ca4dad7f
abdulwali786 <abdulwale622@gmail.com>
2021-10-28 20:52:41 +0500

    functions

---
+++ app.js
@@ -0,0 +1,28 @@
+
+ const run =  (name,email) => {
+    let num1 = 100
+    console.log(name," ",email,num1);
+
+}
+
+run("ahsan","mahsan@gmail.cm")
+ 
+var arr = [1,2,3]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(typeof element);
+}
+
+array.forEach(element => {
+
+    console.log(element+12312);
+});
+let num1 = 12
+console.log("outside ",num1);
+let num1 = 123123
+console.log(num1);
+num1 = 24
+console.log(num1);
+
+
